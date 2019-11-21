48f134e92e9e423d9f8e94058ad44cc92e7d6cc3
Jimmy Hu <jiaming_hu@apple.com>
2019-11-20 20:52:22 -0800

    Add customized tokyo60 keymap

---
+++ keyboards/tokyo60/keymaps/jimmyMac/keymap.c
@@ -0,0 +1,50 @@
+#include QMK_KEYBOARD_H
+
+//#include "keymap_common.h"
+
+#define BL 0
+#define FN 1
+/*
+ * Default HHKB Layout
+ */
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: Default layer
+┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+│ Esc │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │  -  │  =  │  \  │  `  │
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│ Tab │  Q  │  W  │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │BkSpc│█████│
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│ Fn  │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │  '  │█████│Enter│█████│
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│Shift│█████│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │█████│Shift│Ctrl │
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│█████│ Alt │ Gui │█████│█████│Space│█████│█████│█████│█████│█████│ Gui │ Alt │█████│█████│
+└─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+     */
+[BL] = LAYOUT_60_hhkb(
+        KC_ESC,    KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS, KC_GRV, \
+        KC_TAB,    KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,      \
+        MO(FN),   KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,      \
+        KC_LSFT,            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,      KC_RSFT, KC_LCTL, \
+             KC_LALT, KC_LGUI,               KC_SPC,                         KC_RGUI, KC_RALT ),
+    /* 1: HHKB Fn layer
+┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+│ Ejc │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │ Ins │ Del │
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│Caps │ HOM │  U  │ END │ PgU │     │  %  │  &  │  ^  │  _  │  =  │ VoD │ VoU │ Mute│█████│
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│     │  L  │  D  │  R  │ PgD │     │  `  │  ~  │  +  │  -  │  |  │  \  │█████│NPEnt│█████│
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│     │█████│     │ C-A │     │BriD │BriU │ Del │     │ Prv │ Nxt │ P/P │█████│     │
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│█████│     │Ctrl │█████│█████│█████│     │█████│█████│█████│█████│     │     │█████│█████│
+└─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+     */
+[FN]= LAYOUT_60_hhkb(
+       KC_EJCT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL, \
+       KC_CAPS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_TRNS, KC_PERC, KC_AMPR, KC_CIRC, KC_UNDS, KC_PEQL, KC_VOLD, KC_VOLU, KC_MUTE,  \
+       KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, KC_TRNS, KC_GRV, KC_TILD, KC_PPLS, KC_PMNS, KC_PIPE, KC_BSLS, KC_PENT,       \
+       KC_TRNS,       KC_TRNS, LCTL(KC_A), KC_TRNS, KC_BRID, KC_BRIU, KC_DEL, KC_TRNS, KC_MPRV,  KC_MNXT, KC_MPLY,       KC_TRNS, KC_TRNS, \
+             KC_TRNS, KC_LCTL,                   KC_TRNS,                         KC_TRNS, KC_TRNS ),
+};
+
